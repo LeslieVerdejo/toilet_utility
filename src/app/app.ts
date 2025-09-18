16b2e6cac3cf125452c6d626b34cd2c6114788c4
Deneken <deneken3109@gmail.com>
2025-09-18 17:03:51 -0300

    se agrega bootstrap

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Employed } from './pages/employed/employed';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Employed],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('toilet_utility');
