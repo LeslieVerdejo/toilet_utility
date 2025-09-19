b126fde5bd35c021a4b7397c0dd264ee180926d9
Deneken <deneken3109@gmail.com>
2025-09-18 22:07:19 -0300

    se agrega la paginacion

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Employed } from './pages/employed/employed';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Employed],
+  imports: [RouterOutlet, Employed, NgxPaginationModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
